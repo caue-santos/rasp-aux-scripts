5c22fac7a7c1539b228323ee3bad2a9507760641
Caue Santos <caue.sants@icloud.com>
2022-04-24 22:32:48 -0300

    server_id as variable

--- networkSpeedTest.sh
+++ networkSpeedTest.sh
@@ -4,7 +4,10 @@
 # and speedtest-cli:
 # https://www.speedtest.net/apps/cli
 
-download=`speedtest -s 34294 | grep Download | awk {'print $3'}`
+# Server ID - CCS Telecom - to list servers run: speedtest -L
+server_id=34294
+
+download=`speedtest -s $server_id | grep Download | awk {'print $3'}`
 
 if [ $(echo "$download < 500" | bc ) -ne 0 ]; then
     telegramsg "Taxa de download abaixo do contratado: $download"
